5066cc97320362d6252bc3e0ee3251dc29d7c720
h07sec <redmifive64gb@gmail.com>
2026-08-08 12:39:11 +0530

    Update HeroSection.tsx

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -6,7 +6,7 @@ import ContactButton from '../components/ContactButton';
 const NAV_LINKS = ['About', 'Skills', 'Projects', 'Contact'];
 
 const PORTRAIT_URL =
-  'https://shrug-person-78902957.figma.site/_components/v2/d24c01ad3a56fc65e942a1f501eb73db42d7cf9a/Rectangle_40443.81459862.png';
+  'https://imgh.in/host/qh5k4z';
 
 export default function HeroSection() {
   return (
